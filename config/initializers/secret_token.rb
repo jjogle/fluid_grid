4354ded9985808ea438a07e527e2f443fe65bc5d
Josh Ogle <joshua@go-ogle.org>
2013-03-18 09:43:57 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FluidGrid::Application.config.secret_token = '7af4dae1e2cb6fb497520ec79bcbd9d5cd29ec4750fe6608063db8be5dd4be6a537cd8a596a783b8ac15cbc804f799b6d3687e972951168d925a66d4101243b3'
